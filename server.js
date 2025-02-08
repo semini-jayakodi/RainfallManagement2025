d47a495b0971003bb1d0639d16d81b98894f1152
semini-jayakodi <jayakodi.sem17@gmail.com>
2025-02-09 01:32:10 +0530

    server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 // ---------------------------
 mongoose
   .connect(
-    'mongodb+srv://semini:pxeq2tCTl0xOtRSt@cluster0.v3gdb.mongodb.net/?retryWrites=true&w=majority',
+    'mongodb+srv://user1:userpw@cluster0.v3gdb.mongodb.net/?retryWrites=true&w=majority',
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => console.log('Connected to MongoDB'))
