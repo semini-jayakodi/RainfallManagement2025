62312a766470b7f9c027eaa40d3411682d49758b
semini-jayakodi <jayakodi.sem17@gmail.com>
2025-02-09 00:22:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 // ---------------------------
 mongoose
   .connect(
-    'mongodb+srv://Rio:RioAstal1234@rio.kh2t4sq.mongodb.net/myDatabase?retryWrites=true&w=majority',
+    'mongodb+srv://cluster0:Semini@semini.usbsmxc.mongodb.net/?retryWrites=true&w=majority',
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => console.log('Connected to MongoDB'))
